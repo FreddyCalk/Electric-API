795302f2d563fb6db522ce006600f6dd5684e045
Freddy Calk <freddycalk@gmail.com>
2015-12-10 10:42:06 -0500

    Mongo/Node API

---
+++ models/photos.js
@@ -0,0 +1,10 @@
+// models/photos.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var photoSchema = new Schema({
+	image: String,
+	totalVotes: Number
+})
+
+module.exports = mongoose.model('photos', photoSchema)
